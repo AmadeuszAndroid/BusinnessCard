b31202dda58f5fa14fe38bac2beb1692de44f0b1
Amadeusz Lisiecki <amadeusz.lisiecki.1993@gmail.com>
2022-11-26 14:49:15 +0100

    Dodany obraz z logiem Androida.

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -3,15 +3,20 @@ package com.example.businesscard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.businesscard.ui.theme.BusinessCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +27,7 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                 ) {
-                    Greeting("Android")
+                    Greeting()
                 }
             }
         }
@@ -30,11 +35,22 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Column(Modifier
-        .background(colorResource(R.color.navy))
+fun Greeting() {
+    val image = painterResource(R.drawable.android_logo)
+    Column(
+        modifier = Modifier
+            .background(
+                colorResource(R.color.navy)
+            ),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
-        Text(text = "Hello $name!")
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier
+                .size(100.dp)
+        )
     }
 }
 
@@ -42,6 +58,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     BusinessCardTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
